efd5cee19fc2297af1582c01fac12b133d485191
Cody Wilson <cody@codifynw.com>
2020-11-26 02:31:39 -0800

    step into static dir and print filename

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import pathlib
 def walk(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
-            print('crab')
             print(os.path.join(root, name))
         for name in dirs:
             print('folder')
             walk(os.path.join(root, name))
 
-scriptPath = pathlib.Path(__file__).parent.absolute()
-walk(scriptPath)
+htmlPath = pathlib.Path(__file__).parent.absolute()
+htmlPath = htmlPath / "static"
+walk(htmlPath)
