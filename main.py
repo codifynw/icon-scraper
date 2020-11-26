c7b08e7a5620f3ffac909450670f96dac31ed956
Cody Wilson <cody@codifynw.com>
2020-11-26 11:48:07 -0800

    rread and print files one line at a time

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import os
 import pathlib
 
+def scanLine(line):
+    print(line)
+    # print("line {} contents {}".format(cnt, line))
+
+def scanFile(filePath):
+    print('scan')
+    with open(filePath) as fp:
+        for line in fp:
+            scanLine(line)
+
 def walk(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
-            print(os.path.join(root, name))
+            scanFile(os.path.join(root, name))
         for name in dirs:
             print('folder')
             walk(os.path.join(root, name))
