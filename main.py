ab216d6687cf160db0e72dd33c288668a15d8290
Cody Wilson <cody@codifynw.com>
2020-11-26 22:12:45 -0800

    converted to object, writing new files in dist

--- main.py
+++ main.py
@@ -1,55 +1,33 @@
 import os
 import pathlib
+from pathlib import Path
 import re
 from bs4 import BeautifulSoup
 
-def writeResult(line):
-    newFilename.write(line)
+class Parser:
+    def __init__(self, scriptPath):
+        self.definePaths(scriptPath)
+        self.scanStatic()
 
-def updateLine(line, soup):
-    # TODO: add new icon class, remove data-icon
-    # writeResult(line)
-    return("THIS LINE WILL BE CHANGED")
+    def definePaths(self, scriptPath):
+        self.srcPath = scriptPath / "static"
+        self.distPath = scriptPath / "__dist/"
 
-def getClasses(line, soup):
-    return [line["class"] for line in soup.find_all() if "class" in line.attrs]
+    def scanStatic(self):
+        for root, dirs, files in os.walk(self.srcPath, topdown=False):
+            for file in files:
+                if file.split('.')[1] == 'html':
+                    path_file = os.path.join(root,file)
+                    pathInStatic = root.split('icon-scraper/static/')[1]
+                    filePath = os.path.join(self.distPath,pathInStatic)
+                    Path(filePath).mkdir(parents=True, exist_ok=True)
+                    self.newFile = open(os.path.join(filePath,file), "w+")
+                    # self.scanFile(os.path.join(root, name))
 
-def getIconValue(line, soup):
-    return [line["data-icon"] for line in soup.find_all() if "data-icon" in line.attrs]
-
-def lookForIcon(line):
-    if line.find("data-icon") > -1:
-        soup = BeautifulSoup(line, 'html.parser')
-        iconValue = getIconValue(line, soup)
-        classResult = getClasses(line, soup)
-        if classResult:
-            writeResult(updateLine(line, soup))
-        else:
-            print('element has no class - note exception')
-            writeResult(line)
-    else:
-        writeResult(line)
-
-def scanLine(line):
-    lookForIcon(line)
-
-def scanFile(filePath):
-    print(filePath)
-    with open(filePath) as fp:
-        for line in fp:
-            scanLine(line)
-
-def scanDir(path):
-    for root, dirs, files in os.walk(path, topdown=False):
-        for name in files:
-            if name.split('.')[1] == 'html':
-                newFilename = open(name, "w+")
-                scanFile(os.path.join(root, name))
-        for name in dirs:
-            scanDir(os.path.join(root, name))
+    def scanFile(self, line):
+        print('SCAN FILE FUNCTION')
+        # print(self.newFilename)
 
 if __name__ == '__main__':
     scriptPath = pathlib.Path(__file__).parent.absolute()
-    srcPath = scriptPath / "static"
-    distPath = scriptPath / "__dist/"
-    scanDir(srcPath)
+    scanObj = Parser(scriptPath)
