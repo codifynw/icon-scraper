09abc96bc57e1c5115e775388754861a8f707375
Cody Wilson <cody@codifynw.com>
2020-11-27 00:16:57 -0800

    working methods

--- main.py
+++ main.py
@@ -22,11 +22,43 @@ class Parser:
                     filePath = os.path.join(self.distPath,pathInStatic)
                     Path(filePath).mkdir(parents=True, exist_ok=True)
                     self.newFile = open(os.path.join(filePath,file), "w+")
-                    # self.scanFile(os.path.join(root, name))
+                    self.scanFile(os.path.join(root, file))
 
-    def scanFile(self, line):
-        print('SCAN FILE FUNCTION')
-        # print(self.newFilename)
+    def scanFile(self, filePath):
+        with open(filePath) as fp:
+            for line in fp:
+                self.scanLine(line)
+
+    def scanLine(self, line):
+        self.lookForIcon(line)
+
+    def lookForIcon(self, line):
+        if line.find("data-icon") > -1:
+            soup = BeautifulSoup(line, 'html.parser')
+            iconValue = self.getIconValue(line, soup)
+            classResult = self.getClasses(line, soup)
+            if classResult:
+                self.updateLine(line, soup)
+            else:
+                self.addClassToLine(line,soup)
+        else:
+            self.writeResult(line)
+
+    def writeResult(self, line):
+        self.newFile.write(line)
+
+    def addClassToLine(self, line, soup):
+        self.writeResult('THIS LINE WILL BE CHANGED')
+
+    def updateLine(self, line, soup):
+        # TODO: add new icon class, remove data-icon
+        self.writeResult('THIS LINE WILL BE CHANGED')
+
+    def getClasses(self, line, soup):
+        return [line["class"] for line in soup.find_all() if "class" in line.attrs]
+
+    def getIconValue(self, line, soup):
+        return [line["data-icon"] for line in soup.find_all() if "data-icon" in line.attrs]
 
 if __name__ == '__main__':
     scriptPath = pathlib.Path(__file__).parent.absolute()
