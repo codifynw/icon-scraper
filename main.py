c2559dd51908c50f8c3d4f55c8bec8e85ffe87fe
Cody Wilson <cody@codifynw.com>
2020-11-26 21:22:06 -0800

    before making object

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import os
 import pathlib
 import re
+from bs4 import BeautifulSoup
+
+def writeResult(line):
+    newFilename.write(line)
+
+def updateLine(line, soup):
+    # TODO: add new icon class, remove data-icon
+    # writeResult(line)
+    return("THIS LINE WILL BE CHANGED")
+
+def getClasses(line, soup):
+    return [line["class"] for line in soup.find_all() if "class" in line.attrs]
+
+def getIconValue(line, soup):
+    return [line["data-icon"] for line in soup.find_all() if "data-icon" in line.attrs]
 
 def lookForIcon(line):
     if line.find("data-icon") > -1:
-        lst = re.findall(r'"(.*?)(?<!\\)"', line)
-        print(line)
-        print(lst)
+        soup = BeautifulSoup(line, 'html.parser')
+        iconValue = getIconValue(line, soup)
+        classResult = getClasses(line, soup)
+        if classResult:
+            writeResult(updateLine(line, soup))
+        else:
+            print('element has no class - note exception')
+            writeResult(line)
+    else:
+        writeResult(line)
 
 def scanLine(line):
     lookForIcon(line)
 
 def scanFile(filePath):
-    print('scan')
+    print(filePath)
     with open(filePath) as fp:
         for line in fp:
             scanLine(line)
@@ -20,11 +42,14 @@ def scanFile(filePath):
 def scanDir(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
-            scanFile(os.path.join(root, name))
+            if name.split('.')[1] == 'html':
+                newFilename = open(name, "w+")
+                scanFile(os.path.join(root, name))
         for name in dirs:
-            print('folder')
             scanDir(os.path.join(root, name))
 
-htmlPath = pathlib.Path(__file__).parent.absolute()
-htmlPath = htmlPath / "static"
-scanDir(htmlPath)
+if __name__ == '__main__':
+    scriptPath = pathlib.Path(__file__).parent.absolute()
+    srcPath = scriptPath / "static"
+    distPath = scriptPath / "__dist/"
+    scanDir(srcPath)
