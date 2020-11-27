dc15f996bbb9bcfa62dacab9abe39a396071c518
Cody Wilson <cody@codifynw.com>
2020-11-27 13:18:12 -0800

    make inner scan class

--- main.py
+++ main.py
@@ -8,31 +8,37 @@ from iconMap import iconMap
 
 class Parser:
     def __init__(self, scriptPath):
+        self.scan = self.Scan(self)
         self.definePaths(scriptPath)
-        self.scanStatic()
+        self.scan.staticDir()
 
     def definePaths(self, scriptPath):
         self.srcPath = scriptPath / "static"
         self.distPath = scriptPath / "__dist/"
 
-    def scanStatic(self):
-        for root, dirs, files in os.walk(self.srcPath, topdown=False):
-            for file in files:
-                if file.split('.')[1] == 'html':
-                    path_file = os.path.join(root,file)
-                    pathInStatic = root.split('icon-scraper/static/')[1]
-                    filePath = os.path.join(self.distPath,pathInStatic)
-                    Path(filePath).mkdir(parents=True, exist_ok=True)
-                    self.newFile = open(os.path.join(filePath,file), "w+")
-                    self.scanFile(os.path.join(root, file))
+    class Scan:
+        def __init__(self, parser):
+            self.outerParser = parser
+            self.scan = self
 
-    def scanFile(self, filePath):
-        with open(filePath) as fp:
-            for line in fp:
-                self.scanLine(line)
+        def staticDir(self):
+            for root, dirs, files in os.walk(self.outerParser.srcPath, topdown=False):
+                for file in files:
+                    if file.split('.')[1] == 'html':
+                        path_file = os.path.join(root,file)
+                        pathInStatic = root.split('icon-scraper/static/')[1]
+                        filePath = os.path.join(self.outerParser.distPath,pathInStatic)
+                        Path(filePath).mkdir(parents=True, exist_ok=True)
+                        self.outerParser.newFile = open(os.path.join(filePath,file), "w+")
+                        self.scan.file(os.path.join(root, file))
 
-    def scanLine(self, line):
-        self.lookForIcon(line)
+        def file(self, filePath):
+            with open(filePath) as fp:
+                for line in fp:
+                    self.scan.line(line)
+
+        def line(self, line):
+            self.outerParser.lookForIcon(line)
 
     def lookForIcon(self, line):
         if line.find("data-icon") > -1:
