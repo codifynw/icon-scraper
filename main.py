f5ad102940ee4d48b3c1dbfa719bc499876e6aae
Cody Wilson <cody@codifynw.com>
2020-12-02 06:06:23 -0800

    working with inner scripts

--- main.py
+++ main.py
@@ -18,35 +18,14 @@ class Parser:
         self.distPath = scriptPath / "__dist/"
 
     def getAttributes(self, soup):
-        print('     ')
-        print('     ')
-        print('     ')
-        print('     ')
-        print('     ')
-        print('     ')
-        # data = json.loads(s[3].string)
-        # print(data)
-        # print(self.soup)
-        # print('self.soup:', self.soup.find_all(attrs={"name" : "data-icon"}))
-        print('     ')
-        print('     ')
-        print('     ')
-        print('     ')
-        print('     ')
-        print('     ')
         return [line["data-icon"] for line in soup.find_all() if "data-icon" in line.attrs]
 
     def clean(self, soup):
         attributes = self.getAttributes(soup)
-        print('**** ***** START **** *****')
-        print('ATTRIBUTES')
-        print('ATTRIBUTES', attributes)
-        print('ATTRIBUTES')
-        print('**** ***** STOP **** *****')
         if attributes:
             for attribute in list(set(attributes)):
                 newClassName = self.mapIconClassFromAttr(attribute)
-                elementsWithAttribute = self.getElementByAttribute(attribute)
+                elementsWithAttribute = self.getElementByAttribute(attribute, soup)
                 for element in elementsWithAttribute:
                     classes = element.get_attribute_list('class')
                     if newClassName:
@@ -55,10 +34,10 @@ class Parser:
                         else:
                             element['class'] = ' '.join(map(str, element.get_attribute_list('class'))) + ' ' + 'show-icon' + ' ' + 'icon-' + newClassName
                         del element.attrs["data-icon"]
-        self.writeResult()
+        return soup
 
-    def getElementByAttribute(self, attribute):
-        return [item for item in self.soup.find_all(attrs={'data-icon' : attribute})]
+    def getElementByAttribute(self, attribute, soup):
+        return [item for item in soup.find_all(attrs={'data-icon' : attribute})]
 
     class Scan:
         def __init__(self, parser):
@@ -68,32 +47,32 @@ class Parser:
         def staticDir(self):
             for root, dirs, files in os.walk(self.outerParser.srcPath, topdown=False):
                 for file in files:
-                    print('file: ', file)
                     if file.split('.')[-1] == 'html':
                         self.outerParser.createNewFile(root,file)
                         self.scan.file(os.path.join(root, file))
                     else:
-                        # simply copy
                         self.outerParser.simplyCopy(root,file)
-
 
         def file(self, filePath):
             with open(filePath) as file:
                 soup = self.outerParser.makeSoup(file)
-                self.outerParser.clean(soup)
-                s = soup.find_all('script')
-                # innerSoup = self.soup.find_all('script')
-                innerString = s[0].string
-                print('/////////')
-                print(s[0].string)
-                print('/////////')
+                self.outerParser.soup = soup
+                self.outerParser.clean(self.outerParser.soup)
+                self.outerParser.cleanScripts()
+                self.outerParser.writeResult()
+
+    def cleanScripts(self):
+        scriptTags = self.soup.find_all('script')
+        for scriptTag in scriptTags:
+            innerString = scriptTag.string
+            if innerString is not None:
                 innerSoup = BeautifulSoup(innerString, 'html.parser')
-                innerSoup = self.outerParser.clean(innerSoup)
-                print(innerSoup)
+                innerSoup = self.clean(innerSoup)
+                scriptTag.clear()
+                scriptTag.append(innerSoup)
 
     def makeSoup(self, file):
-        self.soup = BeautifulSoup(file, 'html.parser')
-        return self.soup
+        return BeautifulSoup(file, 'html.parser')
 
     def createNewFile(self, root, file):
         pathInStatic = root.split('icon-scraper/originalStatic/')[1]
@@ -138,12 +117,10 @@ class Parser:
                     del tag.attrs[attr]
 
     def find(self, iconValue):
-        for x in iconMap:
-            if 'oldIcon' in x:
-                if x["oldIcon"] == iconValue:
-                    return x["className"]
-            # else:
-                # print('FOUND ELEMENT WITHOUT CODE IN MAP: ', iconValue)
+        if iconValue in iconMap:
+            return iconMap[iconValue]
+        else:
+            print('FOUND ELEMENT WITHOUT CODE IN DICT: ', iconValue)
 
     def mapIconClassFromAttr(self, iconValue):
         if iconValue:
